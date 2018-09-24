9d0e1c3cc58792179856f956cdb82fe62c0440b3
ltubbs <40640842+ltubbs@users.noreply.github.com>
2018-09-24 15:27:42 -0400

    Changed to Brandi's code

--- myList.sh
+++ myList.sh
@@ -1,1 +1,25 @@
-./myList2.sh > myList.html
+#!/bin/bash
+
+list()
+{
+  echo "Current File Path: "${PWD}
+  for entry in ./*; 
+  do
+    echo "<P>"
+      echo $entry
+      echo "</P>"
+  done
+}
+
+cat <<- EOF
+ <html>
+    <head>
+        <title>
+        Directory Listing
+        </title>
+    </head>
+    <body>
+    $(list)
+    </body>
+    </html>
+EOF
