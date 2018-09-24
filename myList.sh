9ca11754a80e3f1c1c7ef1cf46bc0da0cceb255b
ltubbs <40640842+ltubbs@users.noreply.github.com>
2018-09-24 13:36:18 -0400

    Changed to redirect output from myList2.sh to an HTML file

--- myList.sh
+++ myList.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-echo "<HTML>
-	<HEAD>
-		<TITLE>Directory Listing</TITLE>
-	</HEAD>
-	<BODY>"
-		for d in ./*; do
-			echo "<P>"; echo $d; echo "</P>"
-		done
-echo	"</BODY>
-</HTML>"
+./myList2.sh > myList.html
