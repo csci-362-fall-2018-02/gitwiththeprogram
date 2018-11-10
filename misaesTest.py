0c3d0846f8750c87533c653f90f640db073efc5e
misae evans <evansme1@g.cofc.edu>
2018-11-10 15:41:36 -0500

    cleaner test file

--- misaesTest.py
+++ misaesTest.py
@@ -4,19 +4,27 @@ import os
 from subprocess import call
 import pyautogui
 
-pyautogui.typewrite('nvim')
-pyautogui.press('space')
-pyautogui.typewrite('testing.txt')
-pyautogui.press('enter')
-pyautogui.typewrite('wuddup wuddup')
-pyautogui.keyDown('esc')
-pyautogui.keyUp('esc')
-pyautogui.typewrite(':wq!')
-pyautogui.keyDown('enter')
-pyautogui.keyUp('enter')
 
-name = "testing.txt"
-path = "/home/misae"
-for root, dirs, files in os.walk(path):
-    if name in files:
-        print("file found!")
+
+def makeWriteFile():
+    pyautogui.typewrite('nvim')
+    pyautogui.press('space')
+    pyautogui.typewrite('testFile.txt')
+    pyautogui.press('enter')
+
+    pyautogui.press('i')
+    pyautogui.typewrite('beth is a clone ')
+    pyautogui.press('enter')
+    pyautogui.press('esc')
+    pyautogui.typewrite(':wq!')
+    pyautogui.press('enter')
+
+def findFile():
+    name = "testFile.txt"
+    path = "/home/misae"
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            print("file found!")
+
+makeWriteFile()
+findFile();
