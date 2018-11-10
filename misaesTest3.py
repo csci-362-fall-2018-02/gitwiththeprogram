29ab845438802b6123fc53e492700201541c39bc
misae evans <evansme1@g.cofc.edu>
2018-11-10 16:42:19 -0500

    test 3 attempt

---
+++ misaesTest3.py
@@ -0,0 +1,35 @@
+import sys
+import tempfile
+import os
+from subprocess import call
+import pyautogui
+
+# a test opens nvim and doesnt save anything
+#test should show that a file was not made
+
+
+def makeFile():
+
+    pyautogui.typewrite('nvim')
+    pyautogui.press('space')
+    pyautogui.typewrite('justAnotherTest.txt')
+    pyautogui.press('enter')
+    pyautogui.press('esc')
+    pyautogui.typewrite(':q!')
+    pyautogui.press('enter')
+
+def findFile():
+    name = "justAnotherTest.txt"
+    path = "/home/misae"
+    failed = "the file exists: test failure"
+    passed = "the file doesnt exist: test passed"
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            print("file found!")
+
+            return failed
+        else:
+
+            return passed
+makeFile()
+print(findFile())
