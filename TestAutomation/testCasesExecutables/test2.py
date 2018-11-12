3d30ad814bb497d0cdf23ec11724b3eb260f33c3
ltubbs <40640842+ltubbs@users.noreply.github.com>
2018-11-11 22:24:18 -0500

    Refactored, created two files in testCases to use with this

--- TestAutomation/testCasesExecutables/test2.py
+++ TestAutomation/testCasesExecutables/test2.py
@@ -1,43 +1,61 @@
+# Checks that Neovim can add lines to an existing file.
+
 import sys
 import tempfile
 import os
 from subprocess import call
 import pyautogui
-homeDir = os.getenv('HOME')
+import filecmp
 
-#os.chdir("/home/brandi/Desktop/github/gitwiththeprogram/TestAutomation/testCasesExecutables")
-# A test to add lines to a file and check if it wrote correctly
-def readExistingFile():
-    #os.chdir("/home/brandi/Desktop/github/gitwiththeprogram/TestAutomation/scripts")
-    fileToRead = open(homeDir + "/" + "testFile1.txt", "r")
-    existingLines = fileToRead.read()
+# dirPath is the TestAutomation directory (parent directory of
+# parent directory of executing script)
+homeDir = os.getenv('HOME')
+dirPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+def readExistingFile(fileName):
+    fileToRead = open(fileName, "r")
+    existingLines = fileToRead.readlines()
+    fileToRead.close()
     return existingLines
-def addToFile():
-    #os.chdir("/home/brandi/Desktop/github/gitwiththeprogram/TestAutomation/scripts")
+
+
+def addToFile(sampleText, fileName):
+    # Opens fileName
     pyautogui.typewrite('nvim')
     pyautogui.press('space')
-    pyautogui.typewrite(homeDir + '/testFile1.txt')
+    pyautogui.typewrite(fileName)
     pyautogui.press('enter')
-
+    # Writes sample text to file
     pyautogui.press('o')
-    lineWritten = 'beth is just crazy, shes not a clone'
-    pyautogui.typewrite(lineWritten)
+    pyautogui.typewrite(sampleText)
     pyautogui.press('enter')
     pyautogui.press('esc')
-    pyautogui.typewrite(':wq!')
+    pyautogui.typewrite(':wq')
     pyautogui.press('enter')
 
-existingText = readExistingFile()
-addToFile()
 
-def compareLines():
-    #os.chdir("/home/brandi/Desktop/github/gitwiththeprogram/TestAutomation/scripts")
-    failed = "Test2: Failed (the lines do no matc)"
-    passed = "Test2: Passed (lines match)"
-    newText = readExistingFile()
-    if (existingText == newText):
-        return passed
+# Compares two files to see if content is the same
+def compareLines(file1, file2):
+    if (filecmp.cmp(file1, file2)):
+        print("Passed (lines match)")
     else:
-        return failed
-compareLines()
-print(compareLines())
+        print("Failed (the lines do not match)")
+
+
+# Copy test2-append (in testCases) so it can be restored after
+originalFile = readExistingFile(dirPath +
+    "/testCases/test2-append.txt")
+
+# Appends some text to test2-append
+addToFile("Second line.",
+    dirPath + "/testCases/test2-append.txt")
+
+# Compare to file with the correct text
+compareLines(dirPath + "/testCases/test2-correct.txt",
+    dirPath + "/testCases/test2-append.txt")
+
+# Restore original text of test2-append
+f = open(dirPath + "/testCases/test2-append.txt", "w")
+f.writelines(originalFile)
+f.close()
