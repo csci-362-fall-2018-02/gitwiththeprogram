86ed13c22345703c7d739edda13055c557693edf
ltubbs <40640842+ltubbs@users.noreply.github.com>
2018-11-12 12:41:48 -0500

    Changed so it tests the delete a character command (x)

--- TestAutomation/testCasesExecutables/test4.py
+++ TestAutomation/testCasesExecutables/test4.py
@@ -1,35 +1,45 @@
+# Tests the x command to delete a character
+
 import sys
 import tempfile
 import os
 from subprocess import call
 import pyautogui
+import filecmp
 
-# A test to simply make and save a file
-#specifically testing wq
+# dirPath is the TestAutomation directory (parent directory of
+# parent directory of executing script)
+homeDir = os.getenv('HOME')
+dirPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-def makeWriteFile():
+
+# Creates and saves a file containing sampleText
+def makeFile(sampleText, fileName):
+    # Create file in home directory
     pyautogui.typewrite('nvim')
     pyautogui.press('space')
-    pyautogui.typewrite('testFile4.txt')
+    pyautogui.typewrite(homeDir + "/" + fileName)
     pyautogui.press('enter')
-
+    # Enter sampleText
     pyautogui.press('i')
-    pyautogui.typewrite('aw geez rick ')
-    pyautogui.press('enter')
+    pyautogui.typewrite(sampleText)
+    # Enter command mode, delete the last character, save and quit
     pyautogui.press('esc')
+    pyautogui.press('x')
     pyautogui.typewrite(':wq')
     pyautogui.press('enter')
 
-def findFile():
-    found = 0
-    name = "testFile4.txt"
-    path = "."
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            found = 1
-            print("Test4: Passed (file found!)")
-    if (found ==0):
-        print("Test4: Failed (File not found)")
 
-makeWriteFile()
-findFile()
+def compareLines(file1, file2):
+    if (filecmp.cmp(file1, file2)):
+        print("Passed (lines match)")
+    else:
+        print("Failed (the lines do not match)")
+
+
+makeFile("Hello World!", "test4.txt")
+
+# Compare to file with the correct text
+print("Test 4: ", end="")
+compareLines(dirPath + "/testCases/test4-correct.txt",
+    homeDir + "/test4.txt")
