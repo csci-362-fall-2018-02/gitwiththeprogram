42511365001ee8e67db8e2cf2d70677e4697be0d
John Cameron Quinn <camred333@gmail.com>
2018-11-13 13:27:11 -0500

    rebuilt script per specifications

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
-# cd ..
-# cd testCasesExecutables
-# DIR=$(dirname $0)
+# This bash script runs all the tests in the testCasesExecutables folder, and 
+# then outputs to a file in the formal of testResults(counter).txt
+
+COUNTER='1'
+REPORTSFILE='./reports/testResults'$COUNTER'.txt'
+COMPAREFILE='./reports/test'$COUNTER'-correct.txt'
+TEMPFILE='./temp/tempfile'$COUNTER'.txt'
+echo "here"
+while [ -f $REPORTSFILE ]
+do
+   COUNTER=$((COUNTER+1))
+   REPORTSFILE='./reports/testResults'$COUNTER'.txt'
+   COMPAREFILE='./reports/test'$COUNTER'-correct.txt'
+   TEMPFILE='./temp/tempfile'$COUNTER'.txt'
+done
 
 cd testCasesExecutables
-DIR='.'
+echo "Running all tests in folder: " && pwd
+echo "Outputting report to:" $FILE
 
-echo "Running all tests in folder: "$DIR""
-for entry in $DIR/*;
+for entry in *;
 do
   echo
   echo $entry
-  python $entry >> testResults.txt
+  python3 ./../scripts testInterpreter.py $entry > ./../$TEMPFILE
+  if diff ./../$TEMPFILE ./../$COMPAREFILE >/dev/null 2>&1 
+  then
+     echo -n "Test " && echo -n $COUNTER && echo "passed!" > ./../$REPORTSFILE
+  else
+     echo -n "Test " && echo -n $COUNTER && echo "failed." > ./../$REPORTSFILE
+  fi
 done
+cd ..
+
+echo "cleaning temp"
+cd ./temp/
+rm ./*
--- TestAutomation/scripts/testInterpreter.py
+++ TestAutomation/scripts/testInterpreter.py
@@ -2,6 +2,7 @@
 #output a file
 #compare the file to the file they submitted
 import os, sys
+import pyautogui
 
 input_file = open(sys.argv[1])
 content = input_file.read()
