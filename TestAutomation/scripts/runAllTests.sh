5734942b326f33e9e9d2135abc886542c429b8b2
durhambn <durhambn@g.cofc.edu>
2018-11-13 12:32:28 -0500

    not exactly correct

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 # cd ..
 # cd testCasesExecutables
-# DIR=$(dirname $0)
-
+DIR=$(dirname $0)
+echo $DIR
 cd testCasesExecutables
-DIR='.'
+#DIR='.'
+python testInterpreter.py test1-correct.txt >> testResults.txt
+#/testCases/test1.txt
 
-echo "Running all tests in folder: "$DIR""
-for entry in $DIR/*;
-do
-  echo
-  echo $entry
-  python $entry >> testResults.txt
-done
+
+# echo "Running all tests in folder: "$DIR""
+#
+# for entry in $DIR/*;
+# do
+#   echo
+#   echo $entry
+#   python testInterpreter.py $entry >> testResults.txt
+# done
