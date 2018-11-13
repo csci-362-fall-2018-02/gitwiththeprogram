34332624b55b17c7c176b97f102631dfcb481ec9
durhambn <durhambn@g.cofc.edu>
2018-11-13 12:35:34 -0500

    sample. not correct though just an example.

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -2,9 +2,10 @@
 # cd ..
 # cd testCasesExecutables
 DIR=$(dirname $0)
-echo $DIR
+#echo $DIR
 cd testCasesExecutables
 #DIR='.'
+#this is where it should interate through all test files
 python testInterpreter.py test1-correct.txt >> testResults.txt
 #/testCases/test1.txt
 
