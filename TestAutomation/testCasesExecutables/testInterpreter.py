9fecb46ea29dc771bc1ae840abe9d4c8286f9b92
durhambn <durhambn@g.cofc.edu>
2018-11-13 11:24:25 -0500

    Starting point for the new testing script

--- TestAutomation/testCasesExecutables/testInterpreter.py
+++ TestAutomation/testCasesExecutables/testInterpreter.py
@@ -1,3 +1,8 @@
 ##passed in the test file by the user and interprets the commands they want
 #output a file
 #compare the file to the file they submitted
+import os, sys
+
+input_file = open(sys.argv[1])
+content = input_file.read()
+print(content)
