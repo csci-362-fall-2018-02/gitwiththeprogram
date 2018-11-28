bbc370e6085bebc859dc163851aac09796e9c182
durhambn <durhambn@g.cofc.edu>
2018-11-28 15:24:32 -0500

    only looks at the second line for commands

--- TestAutomation/scripts/pocTestInterpreter.py
+++ TestAutomation/scripts/pocTestInterpreter.py
@@ -7,14 +7,15 @@ import pyautogui
 #reads the file passed as an argument
 input_file = open(sys.argv[1])
 temp_file = sys.argv[2] ### the full path of temp file string comes in here
-content = input_file.read()
+content = input_file.readlines()
 print("testInterpreter.py: using temporary file located at:")
 print(temp_file)
 print("and command file stored at:")
 print(sys.argv[1])
 
 #stores each word in the list words
-words = content.split()
+
+words = content[1].split()
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 #iterates through the words list
